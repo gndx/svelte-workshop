4b1abec014be184d2bf088e45289b5fed3b67de6
Oscar Barajas Tavares <oscar@arepa.dev>
2020-03-20 20:36:33 -0500

    I'll explain when you're older!

---
+++ src/components/Main.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { onMount } from "svelte";
+  const API = "https://rickandmortyapi.com/api/character";
+  let data = [];
+  let characters = [];
+
+  onMount(async () => {
+    const response = await fetch(API);
+    data = await response.json();
+    characters = data.results;
+  });
+</script>
+
+<style>
+  .Main {
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-gap: 8px;
+  }
+  figure,
+  img {
+    width: 100%;
+    margin: 0;
+  }
+</style>
+
+<div class="Main">
+  {#each characters as character}
+    <figure>
+      <img src={character.image} alt={character.name} />
+      <figcaption>{character.name}</figcaption>
+    </figure>
+  {:else}
+    <p>Loading...</p>
+  {/each}
+</div>
